24e0a14768872d42908f76e509db54984cc19896
Vo Dinh Phuc <phucvd2512@gmail.com>
2024-07-03 00:27:59 +0700

    add install scripts

---
+++ scripts/install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Update Ubuntu and get standard repository programs
+sudo apt update && sudo apt full-upgrade -y
+
+function install {
+  which $1 &> /dev/null
+
+  if [ $? -ne 0 ]; then
+    echo "Installing: ${1}..."
+    sudo apt install -y $1
+  else
+    echo "Already installed: ${1}"
+  fi
+}
+
+# Install dotfiles manager
+install stow
+# Create symlinks for config
+cd ..
+stow .
+
+# Basics
+install chrome-gnome-shell
+install curl
+install git
+install htop
+install tree
+install vim
+install wget
+install tmux
+
+# Run all scripts in programs/
+for f in scripts/programs/*.sh; do bash "$f" -H; done
+
+# Get all upgrades
+sudo apt upgrade -y
+sudo apt autoremove -y
