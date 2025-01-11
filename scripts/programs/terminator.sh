381acc4a1eebe57a4a6ba0f054ae93c3ab7b965e
vodinhphuc <phucvd2512@gmail.com>
2025-01-11 10:51:31 +0700

    install terminal && miniconda

---
+++ scripts/programs/terminator.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Installing: terminator..."
+which terminator &> /dev/null
+
+if [ $? -ne 0 ]; then
+  echo "Installing Terminator..."
+  sudo apt install terminator
+  sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/terminator 50
+else
+  echo "Already installed: terminator"
+fi
