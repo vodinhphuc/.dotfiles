c90d8c00118c6994c9693ae8f415d8f39b567401
vodinhphuc <phucvd2512@gmail.com>
2025-07-15 23:46:59 +0700

    feat: auto Yes when install

--- scripts/programs/terminator.sh
+++ scripts/programs/terminator.sh
@@ -4,7 +4,7 @@ which terminator &> /dev/null
 
 if ! command -v terminator &> /dev/null; then
   echo "Installing Terminator..."
-  sudo apt install terminator
+  sudo apt install -y terminator
   sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/terminator 50
 else
   echo "Already installed: terminator"
