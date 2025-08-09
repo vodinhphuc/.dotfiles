b524ac598136f176aee9ef99c278941a365cc8df
vodinhphuc <phucvd2512@gmail.com>
2025-08-09 22:37:45 +0700

    set terminator as default

--- scripts/programs/terminator.sh
+++ scripts/programs/terminator.sh
@@ -6,6 +6,10 @@ if ! command -v terminator &> /dev/null; then
   echo "Installing Terminator..."
   sudo apt install -y terminator
   sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/terminator 50
+  # set terminator as default
+  gsettings set org.gnome.desktop.default-applications.terminal exec /usr/bin/terminator
+  gsettings set org.gnome.desktop.default-applications.terminal exec-arg "-x"
+
 else
   echo "Already installed: terminator"
 fi
