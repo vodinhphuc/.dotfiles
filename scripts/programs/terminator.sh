3069998d38ee099164092351e6dabee97b9c60d2
vodinhphuc <phucvd2512@gmail.com>
2025-08-09 23:12:24 +0700

    config terminator

--- scripts/programs/terminator.sh
+++ scripts/programs/terminator.sh
@@ -2,14 +2,37 @@
 
 which terminator &> /dev/null
 
-if ! command -v terminator &> /dev/null; then
-  echo "Installing Terminator..."
-  sudo apt install -y terminator
-  sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/terminator 50
+if command -v terminator &> /dev/null; then
+  #echo "Installing Terminator..."
+  #sudo apt install -y terminator
+  #sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/terminator 50
   # set terminator as default
-  gsettings set org.gnome.desktop.default-applications.terminal exec /usr/bin/terminator
-  gsettings set org.gnome.desktop.default-applications.terminal exec-arg "-x"
+  #gsettings set org.gnome.desktop.default-applications.terminal exec /usr/bin/terminator
+  #gsettings set org.gnome.desktop.default-applications.terminal exec-arg "-x"
+  #mkdir -p ~/.config/terminator
+  #touch ~/.config/terminator/config
+  cat > ~/.config/terminator/config << EOF
+[global_config]
 
+[keybindings]
+[profiles]
+[[default]]
+audible_bell = True
+cursor_color = "#aaaaaa"
+[layouts]
+[[default]]
+[[[window0]]]
+  type = Window
+  parent = ""
+  size = 1920 , 1080
+  position = 100:100
+[[[child1]]]
+  type = Terminal
+  parent = window0
+
+
+[plugins]
+EOF
 else
   echo "Already installed: terminator"
 fi
